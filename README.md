ee250a7ad1cf7e2cced2cb8e33ccfb497c3421d1
Juan Sensio <it@earthpulse.es>
2022-05-01 11:23:44 +0200

    update

--- README.md
+++ README.md
@@ -4,6 +4,50 @@ Lately, for some reason, I am obsessed with software architecture. Probably beca
 
 After some researching I am building this repo to test ideas and develop best practices for Python and Web projects.
 
+## Example
+
+This repo contains a TODO app example. It's objective is to test ideas on software architecture with Python. The app will be accessible via a web UI, API and CLI (for testing how to manage different entry points in the same codebase) and use two different database backends (again, to test how to manage this at the software architecture level).
+
+### A bad example ?
+
+You can find an implementation of such API in [bad_example.py](bad_example.py). You can start the API with the command
+
+```
+uvicorn bad_example:app
+```
+
+And you can interact with it via the interactive documentation at [http://localhost:8000/docs](http://localhost:8000/docs).
+
+> Note: This implementation uses an in-memory database. Also, you'll need to install all the dependencies to make it work (which I deliberately will not mention).
+
+As you can see, the same file contains the API routes, business logic and database access implementation. Is this bad? The answer is... it depends. If our objective is to demo an idea fast and we will be the only developer, it is a good solution, it serves its purpose. But, what if we want to test it to make sure there are no bugs? or add more features? or change the database? or develop it in a team? Then, it is a very bad solution. As a general rule:
+
+- First, make it work
+- Then, make it well
+- Finally, make it efficient
+
+### Backlog
+
+- Users must be able to register with username and password
+- Users must be able to login in order to use the protected user-level functionality
+- Users must be able to create, retrieve, update and delete their TODOs.
+- User must be able to interact with the library via a web API
+- User must be able to interact with the library via a CLI
+- User must be able to interact with the library via a web UI
+
+### Roadmap
+
+- v0: Core python library that allows for user registration and manage TODOs CRUD.
+- v1: API
+- v2: CLI
+- v3: web UI
+
+The system is testable at the following levels:
+
+- unit tests (pytest) for use cases (fast, good for TDD)
+- integration tests (pytest) for API, cli and databases (slow)
+- end to end test (playwright for web ui) for use cases + API/CLI/UI with database (very slow, good for CI/CD)
+
 ## Project structure
 
 ![relations](/pics/relations.png)
@@ -21,21 +65,6 @@ After some researching I am building this repo to test ideas and develop best pr
 ![flow](/pics/flow.png)
 
 ## Example
-
-This repo contains a TODO app example with the following features:
-
-- web ui (sveltekit app)
-- web API (FastAPI)
-- cli (Typer)
-- core library (Python)
-- local database (?)
-- cloud database (firestore)
-
-The system is testable at the following levels:
-
-- unit tests (pytest) for use cases (fast, good for TDD)
-- integration tests (pytest) for API, cli and databases (slow)
-- end to end test (playwright for web ui) for use cases + API/CLI/UI with database (very slow, good for CI/CD)
 
 ## Git
 
--- bad_example.py
+++ bad_example.py
@@ -1,1 +1,129 @@
-# poner aquí la api completa como ejemplo malo
+from datetime import datetime
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List
+import uuid
+
+# fake database
+
+db = {
+    "juan": {
+        "username": "juan",
+        "password": "123",
+        "todos": [{
+            "id": '123',
+            "content": 'test',
+            "createdAt": datetime.now(),
+        }]
+    }
+}
+
+app = FastAPI()
+
+# users section
+
+
+class User(BaseModel):
+    username: str
+    password: str
+    todos: List[dict] = []
+
+
+@app.get("/users/register")
+async def register_user(username: str, password: str):
+    if not username in db:
+        user_dict = {"username": username, "password": password}
+        db[username] = user_dict
+        return User(**user_dict)
+    raise HTTPException(status_code=400, detail="user already exists")
+
+# auth section
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def decode_token(token):
+    username = token
+    if username in db:
+        user_dict = db[username]
+        return User(**user_dict)
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+    user = User(**user_dict)
+    if not form_data.password == user.password:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
+# TODOs section
+
+
+class Todo(BaseModel):
+    id: str = uuid.uuid4()
+    content: str = ''
+    createdAt: datetime = datetime.now()
+
+# create a new todo item
+
+
+@app.post("/todos")
+async def create_todo(content: str, user: User = Depends(get_current_user)):
+    todo = Todo(content=content)
+    db[user.username]["todos"].append(todo.dict())
+    return todo
+
+# retrieve todos
+
+
+@app.get("/todos")
+async def read_todos(user: User = Depends(get_current_user)):
+    return db[user.username]["todos"]
+
+# update a todo item
+
+
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: str, content: str, user: User = Depends(get_current_user)):
+    todo_ix = [t['id'] for t in db[user.username]["todos"]].index(todo_id)
+    if todo_ix == -1:
+        raise HTTPException(status_code=404, detail="todo not found")
+    todo = Todo(**db[user.username]["todos"][todo_ix])
+    todo.content = content
+    db[user.username]["todos"][todo_ix] = todo.dict()
+    return todo
+
+# delete a todo item
+
+
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: str, user: User = Depends(get_current_user)):
+    try:
+        todo_ix = [t['id'] for t in db[user.username]["todos"]].index(todo_id)
+        db[user.username]["todos"].pop(todo_ix)
+        return {'message': 'todo deleted'}
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
